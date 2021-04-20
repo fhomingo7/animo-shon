aac361a3be39ba4802d0490e1e7c2f86e620d473
[Jason] <[jsmching@yahoo.com]>
2021-04-20 22:59:38 +0800

    in progress: loginAdminAccount method

--- app/src/main/java/com/example/Application/Login.java
+++ app/src/main/java/com/example/Application/Login.java
@@ -2,6 +2,7 @@ package com.example.Application;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Database;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,10 +20,18 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity{
     private FirebaseAuth mAuth;
     private EditText emailAddress2, password2;
+    private TextView admin;
+    private TextView notAdmin;
+    private String parentDbName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,18 +50,48 @@ public class Login extends AppCompatActivity {
         });
 
         Button loginButton = (Button)findViewById(R.id.login);
+        parentDbName = "User";
+        admin = (TextView)findViewById(R.id.admin);
+        notAdmin = (TextView)findViewById(R.id.notAdmin);
+        notAdmin.setVisibility(View.GONE);
+
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                admin.setVisibility(View.GONE);
+                notAdmin.setVisibility(View.VISIBLE);
+                loginButton.setText("Admin Log-in");
+                parentDbName = "Admins";
+
+            }
+        });
+
+        notAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notAdmin.setVisibility(View.GONE);
+                admin.setVisibility(View.VISIBLE);
+                loginButton.setText("Log-in");
+                parentDbName = "User";
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                loginUserAccount();
+                if (parentDbName.equals("User")){
+                    loginUserAccount();
+                }
+                else if (parentDbName.equals("Admins")){
+                    loginAdminAccount();
+                }
             }
         });
-        mAuth = FirebaseAuth.getInstance();
     }
 
     private void loginUserAccount() {
-
         String email, password;
         email = emailAddress2.getText().toString();
         password = password2.getText().toString();
@@ -80,6 +120,43 @@ public class Login extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    private void loginAdminAccount(){
+        String username, password;
+        username = emailAddress2.getText().toString();
+        password = password2.getText().toString();
+
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(getApplicationContext(), "Please enter admin username!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        final DatabaseReference RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child(parentDbName).child(username).exists()){
+
+                    Toast.makeText(getApplicationContext(), "Admin Login Successful!", Toast.LENGTH_LONG).show();
+                    return;
+
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Admin Login Failed!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void initializeUI() {
