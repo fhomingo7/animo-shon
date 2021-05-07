398413fefe5c58de1942705b0e3003b6880b130e
Shawn Castillo <shawn_castillo@dlsu.edu.ph>
2021-05-07 23:55:53 +0800

    -Added prevalet.java and Users.java

---
+++ app/src/main/java/com/example/Application/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.Application.Prevalent;
+
+import com.example.Application.Models.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserEmail = "UserEmail";
+    public static final String UserPasswordKey = "UserPassword";
+}
